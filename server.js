91bd5ec1e2a18abe43564aa1d3e55f5e57760148
UTILISATEUR <utilisateur@sabjon.local>
2026-01-12 05:34:09 +0100

    modifffsssezjrpoires

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 require("dotenv").config();
 
-const registerRoute = require("./routes/register");
+const registerRoute = require("./route/register");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
